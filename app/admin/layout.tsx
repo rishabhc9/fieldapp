4b6a6e0411f86ea16be3bc8a930b1c9de702fcdc
Rishabh Chopda <aaditchopda2@gmail.com>
2026-07-04 01:01:10 +0530

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,3 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { isValidAdminSession, ADMIN_COOKIE_NAME } from '@/lib/adminAuth';
-
 export const runtime = 'nodejs';
 
 export default function AdminLayout({
@@ -9,12 +5,5 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = cookies();
-  const session = cookieStore.get(ADMIN_COOKIE_NAME)?.value;
-
-  if (!isValidAdminSession(session)) {
-    redirect('/admin/login');
-  }
-
   return <>{children}</>;
 }
